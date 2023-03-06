934920b6f8eda4e3cecb4d8ca0a02591e04689c0
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-06 11:07:20 +0100

    Add files via upload

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Layout>Homepage</Layout>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/Pages/ProductDetails.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const ProductDetails = () => {
+  return (
+    <Layout>ProductDetails</Layout>
+  )
+}
+
+export default ProductDetails
